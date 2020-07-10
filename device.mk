0a47ac4c5b0dfb7ad946cb92836f2cc0090e3400
bhaskar <arbkm22@gmail.com>
2020-07-10 13:29:34 +0000

    updated vendor to latest oss
    Change-Id: If07532676a5250e804ddb02bd35df15b6cd70cf6

--- device.mk
+++ device.mk
@@ -70,7 +70,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_COPY_FILES += \
-    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+    vendor/rr/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
 PRODUCT_PACKAGES += \
     lineage.biometrics.fingerprint.inscreen@1.0-service.raphael
@@ -170,4 +170,3 @@ PRODUCT_PACKAGES += \
     libnl
 
 PRODUCT_BOOT_JARS += \
-    WfdCommon
